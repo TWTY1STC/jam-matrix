e366a5a7d5044e45ed578672e151677d3a4f9da0
twty1stc <liza.njuguna@gmail.com>
2016-10-06 04:18:59 +0000

    edits to event service

--- scripts/services/EventMetrics.js
+++ scripts/services/EventMetrics.js
@@ -3,29 +3,29 @@
         $rootScope.songPlays = [];
         
         $rootScope.on('song_played', function(event, data){
-            console.log(data);
+            
         });
-    //     return {
-    //         //function that records a metric obj by pushingit to the $rootScope array
-    //         registerSongPlay: function (songObj){
-    //             //add time to event listener
-    //             songObj['playedAt'] = moment(new Date()).format();
-    //             $rootScope.songPlays.push(songObj);
-    //         },
-    //         listSongsPlayed: function () {
-    //             var songs = [];
-    //             angular.forEach($rootScope.songPlays, function(song){
-    //                 songs.push(song.title);
+        return {
+            //function that records a metric obj by pushing it to the $rootScope array
+            registerSongPlay: function (songObj){
+                //add time to event listener
+                songObj['playedAt'] = moment(new Date()).format();
+                $rootScope.songPlays.push(songObj);
+            },
+            listSongsPlayed: function () {
+                var songs = [];
+                angular.forEach($rootScope.songPlays, function(song){
+                    songs.push(song.title);
                     
-    //             });
-    //             console.log(songs);
-    //             return songs;
-    //         }
-    //     };
+                });
+                console.log(songs);
+                return songs;
+            }
+        };
     }
     
     
     angular
         .module('blocJams')
-        .factory('EventMatrics', EventMatrics);
+        .factory('EventMetrics', EventMetrics);
 })();
